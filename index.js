315454825f731bf4c2fc35fc9a456269114c4ad6
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-01 16:23:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here:
+function driversWithRevenueOver(collection, revenue) {
+  return collection.filter(object => object.revenue > revenue)
+}
+
+function driverNamesWithRevenueOver(collection, revenue) {
+  let objects = collection.filter(object => object.revenue > revenue)
+  return objects.map(object => object.name )
+}
+
+function exactMatch(collection, object) {
+  
+}
